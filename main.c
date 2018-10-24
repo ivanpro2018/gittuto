06034ab3a41a5d490fcc1612077023c7171abf9c
ivanpro2018 <36234391+ivanpro2018@users.noreply.github.com>
2018-10-24 15:51:58 +0100

    fonction multiplication() ajouté

--- main.c
+++ main.c
@@ -6,15 +6,17 @@ int somme(int a, int b){
 	return a+b;
 }
 
+<<<<<<< HEAD
 int multiplication(int a, int b){
 	return a*b;
 }
 
 =======
 >>>>>>> parent of 3d83778... fonction somme() ajouté
+=======
+>>>>>>> parent of 1c6774d... fonction multiplication() ajouté
 int main()
 {
 	printf("Hello Git !");
 	return 0;
 }
-
