b9fa158fbccaf1c56e12e6f751d52f98f272b38c
ivanpro2018 <36234391+ivanpro2018@users.noreply.github.com>
2018-10-24 16:06:30 +0100

    Revert "Fonction multiplication ajouté"
    This reverts commit 76d5d424a228e77e44d65419f3ddec0ff7a72826.

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+<<<<<<< HEAD
 int somme(int a, int b){
 	return a+b;
 }
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 int multiplication(int a, int b){
 	return a*b;
 }
 
+=======
+>>>>>>> parent of 3d83778... fonction somme() ajouté
+=======
+>>>>>>> parent of 1c6774d... fonction multiplication() ajouté
+=======
+>>>>>>> parent of 1c6774d... fonction multiplication() ajouté
 int main()
 {
 	printf("Hello Git !");
 	return 0;
 }
-
-
